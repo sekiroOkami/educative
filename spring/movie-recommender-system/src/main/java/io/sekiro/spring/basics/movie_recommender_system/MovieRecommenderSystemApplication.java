4a30ff66848ce09af323dc1f6da3bee7d243bd3d
global <global@mail.com>
2024-09-10 15:25:44 +0700

    first commit

---
+++ spring/movie-recommender-system/src/main/java/io/sekiro/spring/basics/movie_recommender_system/MovieRecommenderSystemApplication.java
@@ -0,0 +1,13 @@
+package io.sekiro.spring.basics.movie_recommender_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRecommenderSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRecommenderSystemApplication.class, args);
+	}
+
+}
